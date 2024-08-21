8b4a32c196278a62b24f6ed0d76b2f40495562f2
snty181 <109747560+snty181@users.noreply.github.com>
2024-08-21 09:43:18 +0200

    Password creator in its first version
    A simple password creator in which you can choose the length.

---
+++ password_creator_v1.py
@@ -0,0 +1,32 @@
+import string, random
+
+#Password creator
+
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+special = list(string.punctuation)
+
+print(len(letters))
+
+print("Write the length of the password: ")
+length = int(input())
+password = ""
+
+for i in range(length):
+    election = random.randint(0, 2)
+    if election == 0:
+        rand = random.randint(0, 51)
+        password = password + letters[rand]
+
+    elif election == 1:
+        rand = random.randint(0,9)
+        password = password + numbers[rand]
+
+    elif election == 2:
+        rand = random.randint(0,31)
+        password = password + special[rand]
+
+print("The final password with " + str(length) + " characters long is: " + password)
+        
+
+    
